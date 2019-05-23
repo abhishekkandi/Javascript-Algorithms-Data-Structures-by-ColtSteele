332b979de865a678fbc068d84a69048073dbd701
abhishekkandi <kandii.abhishek@gmail.com>
2019-05-23 23:40:53 +0530

    Multiple pointer problem solving pattern example - counting unique values in sorted array.

---
+++ JS Snippets/countUniqueValue.js
@@ -0,0 +1,20 @@
+//Input will be array of sorted values 
+//Output should be Count of unique values present in the array
+//It can have negative numbers too
+//For Example, Input - [-2,-1,0,0,1,1,2,2,3], Output - 6 (-2,-1,0,1,2,3)
+
+//TimeComplexity - O(n)
+function countUniqueValues(arr){
+    if(arr.length === 0) return 0;
+
+    let i = 0;
+    for(let j = 1; j < arr.length; j++) {
+            if(arr[i] !== arr[j]){
+                    i++;
+                    arr[i] = arr[j];
+            }
+    }
+    return i + 1;
+}
+
+countUniqueValues([-2,-1,0,0,1,1,2,2,3]);
