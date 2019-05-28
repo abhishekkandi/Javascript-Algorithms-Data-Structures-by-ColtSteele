817034692131d8d8099c0a5c2693c51514f6535e
abhishekkandi <kandii.abhishek@gmail.com>
2019-05-29 00:11:16 +0530

    Sliding Window Pattern example

--- JS Snippets/countUniqueValue.js
+++ JS Snippets/countUniqueValue.js
@@ -9,10 +9,10 @@ function countUniqueValues(arr){
 
     let i = 0;
     for(let j = 1; j < arr.length; j++) {
-            if(arr[i] !== arr[j]){
-                    i++;
-                    arr[i] = arr[j];
-            }
+        if(arr[i] !== arr[j]){
+            i++;
+            arr[i] = arr[j];
+        }
     }
     return i + 1;
 }
---
+++ JS Snippets/maxSubarraySum.js
@@ -0,0 +1,46 @@
+//Naive solution - Time complexity - O(N^2)
+// function maxSubarraySum(arr,subArrSize){
+
+//     if(arr.length < subArrSize){
+//         return null;
+//     }
+
+//     let max = -Infinity;
+//     for(let i = 0; i < arr.length - subArrSize + 1; i++){
+//         let temp = 0;
+//         for(let j = 0; j < subArrSize; j++){
+//             temp += arr[i + j];
+//         }
+
+//         if(temp > max) {
+//             max = temp;
+//         }
+//     }
+
+//     return max;
+// }
+
+//Time complexity - O(n)
+function maxSubarraySum(arr,subArrSize){
+    let maxSum = 0;
+    let tempSum = 0;
+
+    if(arr.length < subArrSize){
+        return null;
+    }
+
+    for(let i = 0; i < subArrSize; i ++){
+        maxSum += arr[i];
+    }
+
+    tempSum = maxSum;
+    for(let i = subArrSize; i < arr.length; i++){
+        tempSum = tempSum - arr[i - subArrSize] + arr[i];
+        maxSum  = Math.max(maxSum,tempSum);
+    }
+
+    return maxSum;
+}
+
+
+maxSubarraySum([4,2,1,6],3)
